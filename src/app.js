1d758bdd5dabf1ceccef6b09b7be26e13b94c164
IvonnaZhang <ivonna.zhang@gmail.com>
2024-09-08 21:49:08 +0800

    update

--- src/app.js
+++ src/app.js
@@ -16,17 +16,16 @@
 
 // io.of('/stream').on('connection', stream);
 
-// server.listen(8090);
-
-
+// server.listen(8091);
 // 将服务器绑定到局域网 IP 地址
+// server.listen(3000, '192.168.31.245');
 let express = require('express');
 let https = require('https');
 let fs = require('fs');
 let app = express();
-let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon');
+let stream = require('./ws/stream');
 
 // 读取 SSL 证书和私钥
 let options = {
@@ -45,13 +44,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// 创建 HTTPS 服务器并赋值给 server 变量
 let server = https.createServer(options, app);
-let io = require('socket.io')(server);
 
 // WebSocket 连接
+let io = require('socket.io')(server);  // 使用已定义的 server 变量
 io.of('/stream').on('connection', stream);
 
-// 创建 HTTPS 服务器并监听端口
-https.createServer(options, app).listen(8090, () => {
+// 服务器监听端口
+server.listen(8090, () => {
     console.log('Server is running on https://localhost:8090');
 });
